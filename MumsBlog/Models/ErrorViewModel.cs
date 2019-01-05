0976385f6a1f1ab7a236eba224a529120862bcd8
David Edeh <davidedeh15@yahoo.com>
2019-01-05 16:31:12 +0000

    Add project files.

---
+++ MumsBlog/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MumsBlog.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
